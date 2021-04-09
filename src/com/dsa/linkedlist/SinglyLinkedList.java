bdebd452c8cb98c0a92e7f2eb6338e38ee606c4f
nareshkadhare <nareshkadhare@gmail.com>
2021-04-09 15:18:40 +0530

    Rotate Method added for Singly List

--- src/com/dsa/linkedlist/SinglyLinkedList.java
+++ src/com/dsa/linkedlist/SinglyLinkedList.java
@@ -110,22 +110,69 @@ public class SinglyLinkedList {
 		}
 	}
 
-	public static void main(String[] args) {
-		addAtHead(new Node(10));
-		addAtHead(new Node(20));
-		addAtHead(new Node(30));
-		addAtHead(new Node(40));
-		addAtTail(new Node(9));
-		addAtTail(new Node(8));
-		display();
-		System.out.println("Searching for 9 " + search(9));
-		System.out.println("Searching for 50 " + search(50));
-		System.out.println("Updating 9 to 900 : " + update(9, 900));
-		System.out.println("Updating 90 to 900 : " + update(90, 900));
-		display();
-		System.out.println("Removing 30 from list : " + removeNode(40));
-		System.out.println("Removing 300 from list : " + removeNode(8));
-		display();
+	public static int listCount() {
+		int count = 1;
+		Node temp = head;
+		while (temp.next != null) {
+			temp = temp.next;
+			count++;
+		}
+		return count;
+	}
 
+	// Rotate List
+	public static void rotate(int elementPosNumber) {
+
+		if (head == null) {
+			System.out.println("Rotation is not posible");
+			return;
+		}
+		
+		int nodePosition = 1;
+		Node temp = head;
+		while (nodePosition != elementPosNumber) {
+			temp = temp.next;
+			nodePosition++;
+		}
+		
+		if (temp == null || temp.next == null) {
+			System.out.println("Rotation is not posible because - Element number is greter than list");
+			return;
+		}
+		
+		Node rotateNode = temp.next;
+		temp.next = null;
+		// 1,2,3,4->null
+		
+		
+		//5,6 -> head
+		Node temp2 = rotateNode;
+		while (temp2.next != null) {
+			temp2 = temp2.next;
+		}			
+		temp2.next = head;
+		head = rotateNode;
+	}
+
+	public static void main(String[] args) {
+		addAtTail(new Node(10));
+		addAtTail(new Node(20));
+		addAtTail(new Node(30));
+		addAtTail(new Node(40));
+		addAtTail(new Node(50));
+		addAtTail(new Node(60));
+		// addAtTail(new Node(9));
+		// addAtTail(new Node(8));
+		// display();
+		// System.out.println("Searching for 9 " + search(9));
+		// System.out.println("Searching for 50 " + search(50));
+		// System.out.println("Updating 9 to 900 : " + update(9, 900));
+		// System.out.println("Updating 90 to 900 : " + update(90, 900));
+		// display();
+		// System.out.println("Removing 30 from list : " + removeNode(40));
+		// System.out.println("Removing 300 from list : " + removeNode(8));
+		display();
+		rotate(5);
+		display();
 	}
 }
